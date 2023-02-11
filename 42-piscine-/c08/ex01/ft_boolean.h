b8370edf7e0747645372afcb06d09a045a1c8c83
YounesMoukhlij <123767564+YounesMoukhlij@users.noreply.github.com>
2023-02-11 22:36:29 +0100

    Add files via upload

---
+++ 42-piscine-/c08/ex01/ft_boolean.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnard <tnard@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/15 11:02:26 by tnard             #+#    #+#             */
+/*   Updated: 2021/07/22 09:25:36 by tnard            ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef int	t_bool;
+
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS FALSE
+
+# define EVEN(i) i % 2 == 0
+
+#endif
