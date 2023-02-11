9d7b1a0d923f8ab700a8f7217025d445853f58c8
YounesMoukhlij <123767564+YounesMoukhlij@users.noreply.github.com>
2023-02-11 22:35:10 +0100

    Add files via upload

---
+++ 42-piscine-/c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnard <tnard@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/08 08:52:59 by tnard             #+#    #+#             */
+/*   Updated: 2021/07/22 09:24:51 by tnard            ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcapitalize(char *str)
+{
+	int		i;
+	int		to_up;
+
+	i = 0;
+	to_up = 1;
+	while (str[i])
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z')
+			|| (str[i] >= 'A' && str[i] <= 'Z')
+			|| (str[i] >= '0' && str[i] <= '9'))
+		{
+			if (to_up && (str[i] >= 'a' && str[i] <= 'z'))
+				str[i] = str[i] - 32;
+			else if (!to_up && (str[i] >= 'A' && str[i] <= 'Z'))
+				str[i] = str[i] + 32;
+			to_up = 0;
+		}
+		else
+			to_up = 1;
+		i++;
+	}
+	return (str);
+}
---
+++ 42-piscine-/c03/ex04/ft_strstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnard <tnard@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/08 16:23:21 by tnard             #+#    #+#             */
+/*   Updated: 2021/07/22 09:24:59 by tnard            ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	n;
+	int	i;
+	int	a;
+
+	n = 0;
+	i = 0;
+	a = 0;
+	if (to_find[i] == '\0')
+		return (str);
+	while (to_find[i])
+		i++;
+	while (str[n])
+	{
+		a = 0;
+		while (str[n] && str[n++] == to_find[a++])
+		{
+			if (to_find[a] == '\0')
+				return (&str[n - a]);
+		}
+	}
+	return (0);
+}
